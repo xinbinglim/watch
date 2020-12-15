e44ddfbd19e8a3489d316a5b9f94947e1f8c82a6
xinbinglim <76046881+xinbinglim@users.noreply.github.com>
2020-12-15 19:55:17 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,66 @@
+input.onButtonPressed(Button.A, function () {
+    if (hours < 23) {
+        hours += 1
+    } else {
+        hours = 0
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    adjust = hours
+    if (ampm) {
+        if (hours > 12) {
+            adjust = hours - 12
+        } else {
+            if (hours == 0) {
+                adjust = 12
+            }
+        }
+    }
+    time = "" + adjust
+    time = "" + time + ":"
+    if (minutes < 10) {
+        time = "" + time + "0"
+    }
+    time = "" + time + minutes
+    if (ampm) {
+        if (hours > 11) {
+            time = "" + time + "PM"
+        } else {
+            time = "" + time + "AM"
+        }
+    }
+    basic.showString(time)
+})
+input.onButtonPressed(Button.AB, function () {
+    ampm = !(ampm)
+})
+input.onButtonPressed(Button.B, function () {
+    if (minutes < 59) {
+        minutes += 1
+    } else {
+        minutes = 0
+    }
+})
+let ampm = false
+let time = ""
+let adjust = 0
+let minutes = 0
+let hours = 0
+hours = 0
+minutes = 0
+adjust = 0
+time = ""
+ampm = false
+basic.forever(function () {
+    basic.pause(60000)
+    if (minutes < 59) {
+        minutes += 1
+    } else {
+        minutes = 0
+        if (hours < 23) {
+            hours += 1
+        } else {
+            hours = 0
+        }
+    }
+})
